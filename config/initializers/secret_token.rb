1f3014d5dbfe2c95dba5486d4a802eca24f9e9ae
Taylor Mock <tmock12@gmail.com>
2014-01-31 16:21:28 -0500

    Rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RaceCalendar::Application.config.secret_key_base = '463155ff48a55db10739965d9bfe8bef859809ddba2fa949a2e96026c73d2a0c80299628285f42b89d67f6b1f28b2f094568b98b38ad4d5c007e435120eed4ec'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RaceCalendar::Application.config.session_store :cookie_store, key: '_race_calendar_session'
