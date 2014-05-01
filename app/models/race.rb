8e7c2a7e79186656f6e3fdf4b605bca1c4b97707
Taylor Mock <tmock12@gmail.com>
2014-05-01 14:23:01 -0400

    Validate races are not duplicates

--- app/models/race.rb
+++ app/models/race.rb
@@ -1,8 +1,16 @@
 class Race < ActiveRecord::Base
   geocoded_by :full_address
+  validate :new_race
   after_validation :geocode
 
   def full_address
     [street, city, state, zip, country].compact.join(', ')
+  end
+
+  private
+
+  def new_race
+    errors.add(:title, "Race already in database") if
+    Race.where(date: date).where("title LIKE ?", "%#{title}%").present?
   end
 end
