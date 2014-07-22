372f69a98067bfb02f4ac561d4f05b054ffe3706
Taylor Mock <tmock12@gmail.com>
2014-07-22 10:21:08 -0400

    Create AuthemSessions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140616182339) do
+ActiveRecord::Schema.define(version: 20140722141739) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
